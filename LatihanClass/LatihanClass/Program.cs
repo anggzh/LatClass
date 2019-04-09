213f0f40c5b07450b0b728aeb2c6107aa95a0e3b
anggzh <45726270+anggzh@users.noreply.github.com>
2019-04-09 15:00:50 +0700

    Add files via upload

---
+++ LatihanClass/LatihanClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time objTime = new Time();
+            Time objTime = new Time(2019,4,9,12,1,2);
+            //memberi nilai property
+            objTime.Year = 2009;
+            objTime.Month = 5;
+            objTime.Date = 1;
+            //mengambil nilai dengan property
+            Console.WriteLine("Year : {0}", objTime.Year.ToString());
+            Console.WriteLine("Month : {0}", objTime.Month.ToString());
+            Console.WriteLine("Date : {0}", objTime.Date);
+            Console.WriteLine("Hour : {0}", objTime.Hour);
+            Console.WriteLine("Minute : {0}", objTime.Minute);
+            Console.WriteLine("Second : {0}", objTime.Second);
+        }
+    }
+}
